e442d196cb81a233220899dbd90444700f0dfbe6
markocopic <41292153+markocopic@users.noreply.github.com>
2018-08-28 14:38:51 +0200

    Add files via upload

---
+++ src/Ninjas.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>Ninjas</h1>
+  <h2>Marko</h2>
+<ul>
+  <li v-for="ime in ninjas">{{ ime }}</li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+  // name: 'app',
+  data(){
+    return{
+      ninjas:['mile','gile','rale']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
